bede99ca448bddab57cad7b425f943653b2ba032
Jokuna <rafik57@g.skku.edu>
2021-04-05 11:21:43 +0900

    [Add] 요일별 할일

--- src/services/date.ts
+++ src/services/date.ts
@@ -2,5 +2,8 @@ import moment from 'moment-timezone';
 
 const NumOfdays = (): number => Number(moment().tz('Asia/Seoul').format('DD'));
 const TodayDate = (): string => moment().tz('Asia/Seoul').format('YY-MM-DD');
+const TodayWeek = (): number => moment().tz('Asia/Seoul').day();
+// 요일
+// 0: 일, 1: 월, 2: 화. 3: 수, 4: 목, 5: 금, 6: 토
 
-export { NumOfdays, TodayDate };
+export { NumOfdays, TodayDate, TodayWeek };
--- src/services/mail_content.ts
+++ src/services/mail_content.ts
@@ -1,4 +1,4 @@
-import { NumOfdays } from './date';
+import { NumOfdays, TodayWeek } from './date';
 
 const MailContent = (TodayMenu, TodaySchedule, MonthlyMenu): string => {
   let text = `[메뉴]\n${TodayMenu}\n\n[복지관 일정]\n${TodaySchedule}`;
@@ -8,18 +8,37 @@ const MailContent = (TodayMenu, TodaySchedule, MonthlyMenu): string => {
   const Menu24: string | undefined = MonthlyMenu.find((e) => e.date === 24)
     ?.menu;
 
-  if (NumOfdays() === 25) {
-    text = `1시간 일찍 퇴근하는 날~\n` + text;
-  } else if (NumOfdays() === 24) {
-    // 24일이 금요일 or 25일 평일 공휴일
-    if (!Menu25) {
+  const week = TodayWeek();
+  const day = NumOfdays();
+
+  // TodayList
+  switch (week) {
+    case 1:
+      text = `도시락 세팅!!!\n` + text;
+      break;
+    case 2:
+      text = `대체식 세팅\n` + text;
+      break;
+    case 3:
+      text = `10시30분부터 12시 업무 지원인지 확인!\n` + text;
+      break;
+  }
+
+  switch (day) {
+    case 25:
       text = `1시간 일찍 퇴근하는 날~\n` + text;
-    }
-  } else if (NumOfdays() === 23) {
-    if (!Menu25 && !Menu24) {
-      // 23일이 금요일 or 24, 25일 공휴일
-      text = `1시간 일찍 퇴근하는 날~\n` + text;
-    }
+      break;
+    case 24:
+      if (!Menu25) {
+        text = `1시간 일찍 퇴근하는 날~\n` + text;
+      }
+      break;
+    case 23:
+      if (!Menu25 && !Menu24) {
+        // 23일이 금요일 or 24, 25일 공휴일
+        text = `1시간 일찍 퇴근하는 날~\n` + text;
+      }
+      break;
   }
   return text;
 };
