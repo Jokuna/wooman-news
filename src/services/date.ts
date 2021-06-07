c9a8f65f15750d2314710cc5fe2ff86e18073ca5
Jokuna <rafik57@g.skku.edu>
2021-06-07 11:19:55 +0900

    [Edi] Error Catch

--- src/services/date.ts
+++ src/services/date.ts
@@ -2,8 +2,10 @@ import moment from 'moment-timezone';
 
 const NumOfdays = (): number => Number(moment().tz('Asia/Seoul').format('DD'));
 const TodayDate = (): string => moment().tz('Asia/Seoul').format('YY-MM-DD');
+const TodaySpecDate = (): string =>
+  moment().tz('Asia/Seoul').format('YY-MM-DD HH:mm');
 const TodayWeek = (): number => moment().tz('Asia/Seoul').day();
 // 요일
 // 0: 일, 1: 월, 2: 화. 3: 수, 4: 목, 5: 금, 6: 토
 
-export { NumOfdays, TodayDate, TodayWeek };
+export { NumOfdays, TodayDate, TodaySpecDate, TodayWeek };
